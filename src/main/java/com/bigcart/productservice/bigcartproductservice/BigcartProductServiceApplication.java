a8490e097ea87e00c120c2cf2e46f3872b0bea3f
Ildephonse BIKINO <ildephonsebikino@Ildephonses-MacBook-Pro.local>
2020-10-19 09:11:59 -0400

    First commit

---
+++ src/main/java/com/bigcart/productservice/bigcartproductservice/BigcartProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bigcart.productservice.bigcartproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigcartProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigcartProductServiceApplication.class, args);
+    }
+
+}
